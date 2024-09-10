9d44b737fa9ebf6588254a913bc29b019810ea6f
susan <susangithaiga26@gmail.com>
2024-09-10 08:52:03 +0300

    Add starter code

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    Start project
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+
+</style>
